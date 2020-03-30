6879049e6450fe617341fa4f3450056a087b9db0
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-30 22:33:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,60 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = Song.new 
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    @@all.find{|song| song.name == song_name}
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
+    end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by!{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    data = filename.split(" - ")
+    artist_name = data[0]
+    song_name_mp3 = data[1].split(".")
+    song_name_no_mp3 = song_name_mp3[0]
+ 
+    song = self.new 
+    song.artist_name = artist_name
+    song.name = song_name_no_mp3
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
 
 end
